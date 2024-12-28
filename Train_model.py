95b11014ab8aa08a0fb5bc3baff820c674519860
Bryan Huynh <bryan.huynh42@gmail.com>
2024-12-27 21:37:53 -0700

    added more things into the config file

--- Train_model.py
+++ Train_model.py
@@ -5,9 +5,11 @@ from torch.utils.data import DataLoader
 from datasets import Dataset, DatasetDict
 import torch
 import json
+import yaml
 from datasets import load_dataset
 from pprint import pprint
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+config = yaml.safe_load(open("./config.yml"))
 
 print(device)
 
@@ -15,11 +17,11 @@ def load_data(filepath):
     with open(filepath) as json_file:
         return json.load(json_file)
 
-train_data = load_data("./spider_data/train_spider.json")
-train_tables_data = load_data("./spider_data/tables.json")
+train_data = load_data(config['spider_training_dataset'])
+train_tables_data = load_data(config['spider_training_tables_dataset'])
 
-test_data = load_data("./spider_data/test.json")
-test_tables_data = load_data("./spider_data/test_tables.json")
+validation_data = load_data(config['spider_validation_dataset'])
+validation_tables_data = load_data(config['spider_validation_tables_dataset'])
 
 
 def generate_table_index(tables):
@@ -29,7 +31,7 @@ def generate_table_index(tables):
     return index_dictionary
 
 train_table_index_dictionary = generate_table_index(train_tables_data)
-test_table_index_dictionary = generate_table_index(test_tables_data)
+validation_table_index_dictionary = generate_table_index(validation_tables_data)
 
 
 
@@ -55,7 +57,7 @@ def parse(entry, tables, table_index_dictionary):
     # print(schema, question, target)
     return {'input': f"Given the following SQL Schema: {schema}. Provide a SQL query reponse for: {question}", 'target': target}
 
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+tokenizer = T5Tokenizer.from_pretrained(config['pretrained_source_model'])
 
 def preprocess_data(examples):
     inputs = [item for item in examples['input']]
@@ -70,22 +72,22 @@ def preprocess_data(examples):
 
 
 raw_training_data = [parse(train_data[i], train_tables_data, train_table_index_dictionary) for i in range(len(train_data))]
-raw_testing_data = [parse(test_data[i], test_tables_data, test_table_index_dictionary) for i in range(len(test_data))]
+raw_validationing_data = [parse(validation_data[i], validation_tables_data, validation_table_index_dictionary) for i in range(len(validation_data))]
 training_dataset = Dataset.from_list(raw_training_data)
-testing_dataset = Dataset.from_list(raw_testing_data)
+validationing_dataset = Dataset.from_list(raw_validationing_data)
 dataset_dict = DatasetDict({
     "train": training_dataset,
-    "test": testing_dataset
+    "validation": validationing_dataset
 })
 encoded_dataset = dataset_dict.map(preprocess_data, batched=True)
 encoded_dataset = encoded_dataset.remove_columns(['input', 'target'])
 
 
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small")
+model = T5ForConditionalGeneration.from_pretrained(config['pretrained_source_model'])
 model.to(device)
 data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
 train_loader = DataLoader(encoded_dataset['train'], batch_size=8, shuffle=True, collate_fn=data_collator)
-eval_loader = DataLoader(encoded_dataset['test'], batch_size=8, shuffle=True, collate_fn=data_collator)
+eval_loader = DataLoader(encoded_dataset['validation'], batch_size=8, shuffle=True, collate_fn=data_collator)
 
 
 # Define training arguments
@@ -94,11 +96,11 @@ eval_loader = DataLoader(encoded_dataset['test'], batch_size=8, shuffle=True, co
 training_args = Seq2SeqTrainingArguments(
     output_dir="./results",
     eval_strategy="steps",
-    eval_steps=250,
-    logging_steps=250,
+    eval_steps=config['evaluation_steps'],
+    logging_steps=config['logging_steps'],
     learning_rate=5e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
+    per_device_train_batch_size=config['batch_size'],
+    per_device_eval_batch_size=config['batch_size'],
     save_total_limit=2,
     predict_with_generate=True,
     remove_unused_columns=True,
@@ -109,7 +111,7 @@ trainer = Seq2SeqTrainer(
     model=model,
     args=training_args,
     train_dataset=encoded_dataset['train'],
-    eval_dataset=encoded_dataset['test'],
+    eval_dataset=encoded_dataset['validation'],
     processing_class=tokenizer,
     data_collator=data_collator
 )
@@ -119,5 +121,5 @@ trainer = Seq2SeqTrainer(
 trainer.train()
 
 # Save the model
-model.save_pretrained("./fine_tuned_t5_sql")
-tokenizer.save_pretrained("./fine_tuned_t5_sql")
+model.save_pretrained(config['model_save_location'])
+tokenizer.save_pretrained(config['model_save_location'])
