0824d5ff536e946734f760cddd8bc6089076ed7b
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-09-22 23:50:13 -0600

    first commit

---
+++ text2sql-springboot/src/main/java/com/text2sql/text2sql_springboot/Entities/UserDetails.java
@@ -0,0 +1,40 @@
+package com.text2sql.text2sql_springboot.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "USER_DETAILS", schema = "public")
+public class UserDetails {
+    @Id
+    @NotBlank
+    @Column(nullable = false, length = 255)
+    private String id;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, length = 255)
+    private String email;
+
+
+    public UserDetails() {
+    }
+
+    public UserDetails(String id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
