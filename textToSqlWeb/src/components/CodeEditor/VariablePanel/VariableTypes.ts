71314fcfe1d10c235da9255376859945e60464c9
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-09-24 22:57:12 -0600

    updates and retreieves the variables to the backend within the variable editing panel

---
+++ textToSqlWeb/src/components/CodeEditor/VariablePanel/VariableTypes.ts
@@ -0,0 +1,14 @@
+export const VariableTypes = [
+	"boolean",
+	"date",
+	"integer",
+	"text",
+	"timestamp",
+	"varchar(n)",
+	"bigint",
+	"decimal(p,s)",
+	"uuid",
+	"serial",
+	"char(n)",
+	"time",
+];
--- textToSqlWeb/src/components/CodeEditor/code-editor-panel.tsx
+++ textToSqlWeb/src/components/CodeEditor/code-editor-panel.tsx
@@ -1,14 +1,19 @@
 import { AppBar, Grid, Paper, Toolbar, Typography } from "@mui/material";
 import CodeIcon from "@mui/icons-material/Code";
 import { TablesPanel } from "./tables-panel";
-import { AddtributeEditorPanel } from "./attribute-editor-panel";
+import { VariableEditorPanel } from "./VariablePanel/attribute-editor-panel";
 import type { UserDatabase } from "../../services/database/user-databases";
+import { useSelector } from "react-redux";
+import type { ActivePanelsType } from "../../reducer/slices/activePanelsSlice";
 
 interface CodeEditorPanelProps {
 	database: UserDatabase;
 }
 
 export const CodeEditorPanel = ({ database }: CodeEditorPanelProps) => {
+	const selectedTable = useSelector(
+		(state: { activePanels: ActivePanelsType }) => state.activePanels.table
+	);
 	return (
 		<Paper sx={{ display: "flex", flexDirection: "column", flex: 1, height: "100%" }}>
 			<AppBar position="static" color="primary">
@@ -16,15 +21,15 @@ export const CodeEditorPanel = ({ database }: CodeEditorPanelProps) => {
 					<CodeIcon fontSize="small" />
 					<Typography variant="subtitle1" color="inherit" component="div" sx={{ px: 1 }}>
 						{database.filename}
-					</Typography>
+					</Typography>	
 				</Toolbar>
 			</AppBar>
 			<Grid container spacing={2} sx={{ p: 2, height: "95%" }}>
 				<Grid size={3} sx={{ height: "100%" }}>
-					<TablesPanel database={database}/>
+					<TablesPanel database={database} />
 				</Grid>
 				<Grid size={9}>
-					<AddtributeEditorPanel />
+					{selectedTable && <VariableEditorPanel table={selectedTable} />}
 				</Grid>
 			</Grid>
 		</Paper>
