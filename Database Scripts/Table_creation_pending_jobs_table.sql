6c2185e1962147db1fa86436eb59751f5cf4926c
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-11-08 19:14:54 -0700

    created pending jobs table, entity and repository

--- Database Scripts/Table_creation_pending_jobs_table.sql
+++ Database Scripts/Table_creation_pending_jobs_table.sql
@@ -1,7 +1,9 @@
+create type JobStatus as ENUM ('STARTED', 'SUCCESS', 'FAILED');
+
 create table pending_jobs (
 	correlation_Id UUID PRIMARY KEY,
 	user_Id VARCHAR(255) NOT NULL,
-	job_Status BOOLEAN NOT NULL,
+	job_Status JOBSTATUS NOT NULL,
 	created_Date TIMESTAMPTZ NOT NULL,
 	updated_Date TIMESTAMPTZ NOT NULL,
 	CONSTRAINT fk_user
